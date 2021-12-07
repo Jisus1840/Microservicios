389f651445f881a4ff215c8fecb5b0c4b29bc21e
Jisus1840 <delacruzjesus083@gmail.com>
2021-12-06 22:35:06 -0600

    Agregando parte del contenedor C, falta agregar perritos y las imagenes de perritos y personas

---
+++ Base-de-datos/conexion.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+def conexion():
+# Creando conexion
+    cliente = MongoClient("mongodb://admin:admin3231@localhost:27017/")
+    db = cliente.dogs
+    mydb = cliente['dogs']
+    # mycol = mydb['personas']
+
+    return mydb
